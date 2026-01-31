07cc095601756665af8d78232341ed84b7154e97
5taZ <stasyfreak@gmail.com>
2026-01-31 20:30:54 +0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -14,7 +14,7 @@ const app = express();
 // ==========================================
 
 app.use(cors({
-  origin: ['hhttps://peppy-frangollo-b305dd.netlify.app/', 'http://localhost:5173', 'http://localhost:3000'],
+  origin: ['hhttps://peppy-frangollo-b305dd.netlify.app', 'http://localhost:5173', 'http://localhost:3000'],
   methods: ['GET', 'POST', 'PATCH', 'DELETE'],
   allowedHeaders: ['Content-Type', 'X-Telegram-Init-Data'],
   credentials: true
